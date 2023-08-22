21d796cf9307b998c3def467c4548d090dd87e03
John Zhu <johnszhu@live.unc.edu>
2023-08-22 15:15:42 -0400

    added cost function linear regression

--- linear/linear_regression.py
+++ linear/linear_regression.py
@@ -1,1 +1,29 @@
 import numpy as np
+
+class Linear_Regression():
+    
+    def calculate_cost(X, Y_hat, W, b):
+        '''
+        Calculates the means squared error (MSE)
+        Inputs
+         - X:     Input Data
+         - Y_hat: True labels
+         - W:     Weights
+         - b:     Bias
+        '''
+        Y = np.dot(X, W) + b
+        return np.sum((Y_hat - Y) ** 2) / (2 * X.shape[0])
+
+    def linear_regression():
+        return
+    
+    def predict():
+        return
+    
+    
+
+
+class linear_test():
+
+    def testfunction():
+        print("Test Success")
---
+++ tests/test_linear_regression.py
@@ -0,0 +1,8 @@
+import unittest
+import sys
+sys.path.append('../ML_from_scratch')
+
+from linear.linear_regression import linear_test
+from linear.linear_regression import Linear_Regression
+
+linear_test.testfunction()
