39d746137b4c341ccdf7d553089538c411760818
John Zhu <johnszhu@live.unc.edu>
2023-08-22 21:41:56 -0400

    added linear_regression code, no testing

--- linear/linear_regression.py
+++ linear/linear_regression.py
@@ -1,8 +1,18 @@
 import numpy as np
 
 class Linear_Regression():
-    
-    def calculate_cost(X, Y_hat, W, b):
+
+    def shape_validation(self, X, Y_hat, W, b):
+        '''
+        Validates the matrix shapes (helper)
+        '''
+        if X.shape[1] != W.shape[0]:
+            raise ValueError("X and W shape Mismatch")
+        if Y_hat.shape[0] != X.shape[0]:
+            raise ValueError("X and Y_hat shape Mismatch")
+        return
+
+    def calculate_cost(self, X, Y_hat, W, b,):
         '''
         Calculates the means squared error (MSE)
         Inputs
@@ -11,15 +21,61 @@ class Linear_Regression():
          - W:     Weights
          - b:     Bias
         '''
-        Y = np.dot(X, W) + b
-        return np.sum((Y_hat - Y) ** 2) / (2 * X.shape[0])
+        self.shape_validation(X, Y_hat, W, b)
 
-    def linear_regression():
-        return
+        Y = np.dot(X, W) + b
+        cost = np.sum((Y_hat - Y) ** 2) / (2 * X.shape[0])
+        return cost
+
+    def gradient_descent(self, X, Y_hat, W, b, learning_rate):
+        '''
+        Performs Gradient Descent on Weight and bias
+        Inputs
+         - X:     Input Data
+         - Y_hat: True labels
+         - W:     Weights
+         - b:     Bias
+        '''
+
+        self.shape_validation(X, Y_hat, W, b)
+
+        m = X.shape[0]
+        Y = np.dot(X, W) + b
+
+        errors = Y - Y_hat
+        dW = np.sum(np.dot(errors, X.T)) / m
+        db = np.sum(errors) / m 
+        W = W - learning_rate * dW
+        b = b - learning_rate * db
+        return W, b
+
+    def predict(self, X, W, b):
+        return np.dot(X, W) + b
+
+    def fit_model(self, X, Y_hat, learning_rate, iterations):
+        '''
+        Performs Linear regression by gradient descent
+        Input:
+        - X: Input values
+        - Y_hat: truth values
+        - learning_rate: alpha
+        '''
+        m = X.shape[0]
+        # Randomly Initialize weights
+        W = np.random.randn(X.shape[1])
+        b = 0.5
+        
+        # blank list to store cost
+        cost_list = []
+
+        for i in range(iterations):
+            W, b = self.gradient_descent(X, Y_hat, W, b, learning_rate)
+            cost = self.calculate_cost(X, Y_hat, W, b)
+            cost_list.append(cost)
+
+        return W, b, cost_list
     
-    def predict():
-        return
-    
+
     
 
 
--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -1,8 +1,42 @@
 import unittest
 import sys
+import numpy as np
 sys.path.append('../ML_from_scratch')
 
 from linear.linear_regression import linear_test
 from linear.linear_regression import Linear_Regression
+
+class TestLinearRegression(unittest.TestCase):
+    
+    # Testing basic calculation of cost. 
+    def test_calculate_cost(self):
+
+        X = np.array([[1, 2], [3, 4]])
+        Y_hat = np.array([1, 2]) 
+        W = np.array([[1], [2]])
+        b = np.array([1])
+
+        lr = Linear_Regression()
+        cost = lr.calculate_cost(X, Y_hat, W, b)
+        expected_cost = 65.5
+        self.assertEqual(cost, expected_cost)
+
+    # Test case 2
+    def test_predict(self):
+        X = np.array([[1,2,3], [4,5,6], [7,8,9]])
+        Y = np.array([[1,2,1], [2,4,6], [7,2,5]])
+
+        lr = Linear_Regression()
+
+        result = lr.predict(X, Y, 0)
+        expected = np.array([[26, 16, 28], [56, 40, 64], [86, 64, 100]])
+
+        np.testing.assert_array_equal(result, expected)
+
+    # Test case 3
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 linear_test.testfunction()
